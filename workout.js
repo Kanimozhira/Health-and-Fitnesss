a5061146bf6e709f8c4bf36111bd3c69ee9c6848
Kanimozhira <147414696+Kanimozhira@users.noreply.github.com>
2024-06-25 20:14:43 +0530

    Add files via upload

---
+++ workout.js
@@ -0,0 +1,16 @@
+// models/Workout.js
+const mongoose = require('mongoose');
+
+const workoutSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  type: { type: String, required: true },
+  duration: { type: Number, required: true },
+  caloriesBurned: { type: Number, required: true },
+  date: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
